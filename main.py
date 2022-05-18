4147441fcdd569417e97c0f71ef2374abc277ec6
7Danilka7 <xamzin_2003@mail.ru>
2022-05-18 20:30:55 +0300

    готов шифровальщик2.0

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
+import random
+
 print("Добро пожаловать, я робот шифровальщик.")
+
+
+def letter(k, text):
+    blocksize = len(k)
+    n = len(text)
+    if n % blocksize != 0:
+        for i in range(blocksize - (n % blocksize)):
+            text += str(chr(random.randrange(ord('a'), ord('z'), 1)))
+    n = len(text)
+    new = ''
+    code = ''
+    for i in range(0, n, blocksize):
+        new = [text[i+j] for j in range(blocksize)]
+        for j in range(blocksize):
+            code += str(new[blocksize - int(k[j]) - 1])
+    print(code)
+
+def group(k, text):
 
 
 def cipher():
     text = input("Введите сообщение: ")
-    n = len(text)
-    print("     Ключ - перестановка чисел\n     Вам нужно ввести числа от 0 до", n-1, "\n     Например для текста из 4 символов подойдет ключ 1 3 0 2")
+    print("     Ключ - перестановка чисел, с помощью которой будет шифроваться ваш текст\n     Например, <<1 3 0 2>>")
     key = input("Введите ключ: ")
     k = key.split(" ")
-    new = ""
-    for i in range(n):
-        new += str(text[n - int(k[i]) - 1])
-    print(new)
-
+    print("     1 - посимвольное шифрование\n     2 - шифрование группы\n     3 - шифрование слов")
+    how = int(input("Способ шифровки: "))
+    if how == 1:
+        letter(k, text)
+    if how == 2:
+        group(k, text)
 
 print("     Вот что я умею:\n     1 - зашифровать\n     2 - расшифровать")
 move = int(input("Пожалуйста, выберите действие: "))
 if move == 1:
     cipher()
 else:
-    print(1)
+    pass
 
 text = 0
 
